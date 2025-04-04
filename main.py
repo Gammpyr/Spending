ee93bd7d24c2aa8ae2e8240c6b26bc9af6e191f3
Gammpyr <malak93@yandex.ru>
2025-04-04 14:56:52 +0300

    update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ spending = {
     4: [],
     5: [],
     6: [],
+    7: [],
     0: [],
 }
 category_code = {
@@ -25,10 +26,11 @@ category_code = {
     4: "Дивиденды",
     5: "Аренда",
     6: "Доставка",
+    7: "Поставщики",
     0: "Прочее"
 }
 
-month = input('Введите месяц: ').title()
+month = input('\nВведите месяц: ').title()
 
 
 @print_generator
@@ -42,28 +44,64 @@ def help_info():
 def get_user_input():
     """Получает информацию и создаёт словарь с расходами по каждой категории"""
     while True:
-        user_key = input('Ввод катег')  # нужен int
+        help_info()
+        user_key = input('\nВвод категории: ')
+        if user_key == '=':
+            print_result()
+            continue
+        if not user_key.isdigit() and len(user_key) != 1:
+            print("\nВведите код:")
+            continue
         if user_key.lower() in ['q', 'й']:
             break
         elif user_key.lower() in ['?', 'h', 'help', 'п', 'помощь']:
-            help_info(category_code)
+            help_info()
         else:
-            user_value = int(input('Ввод суммы'))
-            spending[int(user_key)].append(user_value)
+            while True:
+                user_value = input('Ввод суммы:     ')
+                if user_value.isdigit():
+                    spending[int(user_key)].append(int(user_value))
+                    break
+                else:
+                    print('Введите сумму цифрами!')
 
 
 @print_generator
+def print_result():
+    """Выводим результат"""
+    print(month)
+    for key, value in spending.items():
+        print(f'{category_code[key].ljust(13)} - {sum(value)}')
+    print(f'\nВсего:         - {get_summ()}')
+
+
+def write_result_dict():
+    """Записываем результат как словарь"""
+    with open(f'data/{month}_dict.txt', 'w', encoding='UTF-8') as file:
+        file.write(f'{str(spending)}\n')
+        for key, value in spending.items():
+            file.write(f'{key} : {value}\n')
+
+
 def write_result():
     """Записывает результат в data/название_месяца.txt """
     with open(f'data/{month}.txt', 'w', encoding='UTF-8') as file:
-        print(month)
         file.write(f'{month.upper()}\n')
         for key, value in spending.items():
-            print(f'{category_code[key].ljust(13)} - {sum(value)}')
             file.write(f'{category_code[key].ljust(13)} - {sum(value)}\n')
+        file.write(f'\nВсего:         - {get_summ()}\n\n')
+
+
+def get_summ():
+    """Получаем итоговую сумму расходов"""
+    result = 0
+    for key, value in spending.items():
+        result += sum(value)
+    return result
 
 
 if __name__ == '__main__':
-    help_info()
     get_user_input()
     write_result()
+    write_result_dict()
+    print_result()
